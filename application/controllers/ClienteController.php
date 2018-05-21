8ed6e8ccba9c9323e0f92ccb62452bd173d37949
Luiz Nunes <luizhgnunes@ymail.com>
2018-05-20 21:07:07 -0300

    Adicionado o método de inserção de cliente.

--- application/controllers/ClienteController.php
+++ application/controllers/ClienteController.php
@@ -1,18 +1,18 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class AtendimentoController extends CI_Controller {
+class ClienteController extends CI_Controller {
     
     public function __construct() {
         parent::__construct();
-        $this->load->model('dal/AtendimentoDal');
+        $this->load->model('dal/ClienteDal');
         $this->load->model('entities/Atendimento');
     }
     
     public function inserir() {
-        $atendimento = json_decode(file_get_contents('php://input'));
-        $atendimentoGateway = new AtendimentoDal();
-        echo $atendimentoGateway->inserir($atendimento);
+        $cliente = json_decode(file_get_contents('php://input'));
+        $atendimentoGateway = new ClienteDal();
+        echo $atendimentoGateway->inserir($cliente);
     }
     
 }
--- application/models/dal/ClienteDal.php
+++ application/models/dal/ClienteDal.php
@@ -3,66 +3,37 @@
 // DAL: Data Access Layer
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class AtendimentoDal extends CI_Model {
+class ClienteDal extends CI_Model {
 
-    public function inserir($atendimento_json) {
+    public function inserir($cliente_json) {
         try {
-            
-            $atendimento = Atendimento::fromJson($atendimento_json);
+            $cliente = Cliente::fromJson($cliente_json);
             
             // Conectando ao banco de dados
             $this->load->database();
-
-            $sql = 'INSERT INTO tb_atendimento (';
-            $sql .= 'cli_id, ';
-            $sql .= 'pro_id, ';
-            $sql .= 'ser_id, ';
-            $sql .= 'atd_data_agendado, ';
-            $sql .= 'atd_data_realizado, ';
-            $sql .= 'atd_data_cadastro, ';
-            $sql .= 'atd_endereco, ';
-            $sql .= 'atd_bairro, ';
-            $sql .= 'atd_cep, ';
-            $sql .= 'mun_id, ';
-            $sql .= 'atd_status, ';
-            $sql .= 'atd_preco, ';
-            $sql .= 'atd_desconto, ';
-            $sql .= 'atd_custo_adicional, ';
-            $sql .= 'atd_situacao_pagamento, ';
-            $sql .= 'atd_custo_traNsporte, ';
-            $sql .= 'atd_observacao, ';
-            $sql .= 'atd_avaliacao_data, ';
-            $sql .= 'atd_avaliacao_satisfacao, ';
-            $sql .= 'atd_avaliacao_comentario ';
-            $sql .= ') VALUES (';
-            $sql .= $this->db->escape($atendimento->getCliente()->getId()).', ';
-            $sql .= $this->db->escape($atendimento->getProfissional()->getId()).', ';
-            $sql .= $this->db->escape($atendimento->getServico()->getId()).', ';
-            $sql .= $this->db->escape($atendimento->getDataAgendado()).', ';
-            $sql .= $this->db->escape($atendimento->getDataRealizado()).', ';
-            $sql .= $this->db->escape($atendimento->getDataCadastro()).', ';
-            $sql .= $this->db->escape($atendimento->getEndereco()).', ';
-            $sql .= $this->db->escape($atendimento->getBairro()).', ';
-            $sql .= $this->db->escape($atendimento->getCep()).', ';
-            $sql .= $this->db->escape($atendimento->getMunicipio()->getId()).', ';
-            $sql .= $this->db->escape($atendimento->getStatus()).', ';
-            $sql .= $this->db->escape($atendimento->getPreco()).', ';
-            $sql .= $this->db->escape($atendimento->getDesconto()).', ';
-            $sql .= $this->db->escape($atendimento->getCustoAdicional()).', ';
-            $sql .= $this->db->escape($atendimento->getSituacao()).', ';
-            $sql .= $this->db->escape($atendimento->getCustoTransporte()).', ';
-            $sql .= $this->db->escape($atendimento->getObservacao()).', ';
-            $sql .= $this->db->escape($atendimento->getAvaliacaoData()).', ';
-            $sql .= $this->db->escape($atendimento->getAvaliacaoSatisfacao()).', ';
-            $sql .= $this->db->escape($atendimento->getAvaliacaoComentario());
-            $sql .= '); ';
-            //$sql .= 'SELECT LAST_INSERT_ID();';
             
-            //die($sql);
-            $query = $this->db->query($sql);
-            
-            //return $query->result()[0];
-            return true;
+            $data = array (
+                'cli_nome' => $cliente->getNome(),
+                'cli_login' => $cliente->getLogin(),
+                'cli_senha' => $cliente->getSenha(),
+                'cli_data_nascimento' => $cliente->getDataNascimento(),
+                'cli_sexo' => $cliente->getSexo(),
+                'cli_email' => $cliente->getEmail(),
+                'cli_ddd' => $cliente->getDdd(),
+                'cli_telefone' => $cliente->getTelefone(),
+                'cli_endereco' => $cliente->getEndereco(),
+                'cli_bairro' => $cliente->getBairro(),
+                'cli_cep' => $cliente->getCep(),
+                'mun_id' => $cliente->getMunicipio()->getId(),
+                'cli_ultimo_acesso' => '0000-00-00 00:00:00',
+                'cli_uniqid' => 'abcdf',
+                'cli_data_confirmacao' => '0000-00-00 00:00:00'
+            );
+             
+            //$str = $this->db->insert_string('tb_cliente', $data);
+            //die(print_r($str));
+            $this->db->insert('tb_cliente', $data);
+            return $this->db->insert_id();
             
         } finally {
             if (isSet($this->db))
