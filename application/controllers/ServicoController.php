cb501b72c721f19c5cbd40d9685bf5aa3cda2767
Filipe de Sousa Botelho <filipegame07@gmail.com>
2018-06-12 08:05:11 -0300

    Permissão do CORS a todos no ServicoController

--- application/controllers/ServicoController.php
+++ application/controllers/ServicoController.php
@@ -2,22 +2,23 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class ServicoController extends CI_Controller {
-    
+
     public function __construct() {
         parent::__construct();
+        $this->output->set_header('Access-Control-Allow-Origin: *');
         $this->load->model('dal/ServicoDal');
         $this->load->model('entities/Servico');
     }
-    
+
     public function inserir() {
         $servico = json_decode(file_get_contents('php://input'));
         $servicoGateway = new ServicoDal();
         echo $servicoGateway->inserir($servico);
     }
-    
+
     public function listarTodos() {
         $servicoGateway = new ServicoDal();
         echo json_encode($servicoGateway->listarTodos());
     }
-    
+
 }
