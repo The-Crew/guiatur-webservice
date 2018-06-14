8f28d4122b16c13b09f47b228eee7db7f58d9507
Filipe de Sousa Botelho <filipegame07@gmail.com>
2018-06-14 09:37:37 -0300

    Permissão do CORS a todos no ClienteController

--- application/controllers/ClienteController.php
+++ application/controllers/ClienteController.php
@@ -2,28 +2,29 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class ClienteController extends CI_Controller {
-    
+
     public function __construct() {
         parent::__construct();
+        $this->output->set_header('Access-Control-Allow-Origin: *');
         $this->load->model('dal/ClienteDal');
         $this->load->model('entities/Atendimento');
     }
-    
+
     public function inserir() {
         $cliente = json_decode(file_get_contents('php://input'));
         $atendimentoGateway = new ClienteDal();
         echo $atendimentoGateway->inserir($cliente);
     }
-    
+
     public function logar() {
         $cliente = json_decode(file_get_contents('php://input'));
         $clienteGateway = new ClienteDal();
         echo json_encode($clienteGateway->logar($cliente), JSON_PRETTY_PRINT);
     }
-    
+
     public function obterQuantidade() {
         $clienteGateway = new ClienteDal();
         echo $clienteGateway->obterQuantidade();
     }
-    
+
 }
