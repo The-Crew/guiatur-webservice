a8633cb5382e75ed7b2eda0ae8edd61da353731c
Filipe de Sousa Botelho <filipegame07@gmail.com>
2018-06-14 22:50:17 -0300

    Adicionado o método de cancelar agendamento recebendo o id do agendamento como parâmetro

--- application/controllers/AtendimentoController.php
+++ application/controllers/AtendimentoController.php
@@ -33,4 +33,10 @@ class AtendimentoController extends CI_Controller {
         echo json_encode($atendimentoGateway->listarConcluidos($cliente), JSON_PRETTY_PRINT);
     }
 
+    public function cancelar() {
+        $atendimento = json_decode(file_get_contents('php://input'));
+        $atendimentoGateway = new AtendimentoDal();
+        echo json_encode($atendimentoGateway->cancelar($atendimento), JSON_PRETTY_PRINT);
+    }
+
 }
--- application/models/dal/AtendimentoDal.php
+++ application/models/dal/AtendimentoDal.php
@@ -7,12 +7,12 @@ class AtendimentoDal extends CI_Model {
 
     public function inserir($atendimento_json) {
         try {
-            
+
             $atendimento = Atendimento::fromJson($atendimento_json);
-            
+
             // Conectando ao banco de dados
             $this->load->database();
-            
+
             $data = array (
                 'cli_id' => $atendimento->getCliente()->getId(),
                 'pro_id' => $atendimento->getProfissional()->getId(),
@@ -30,16 +30,16 @@ class AtendimentoDal extends CI_Model {
                 'atd_custo_transporte' => $atendimento->getCustoTransporte(),
                 'atd_observacao' => $atendimento->getObservacao()
             );
-             
+
             $this->db->insert('tb_atendimento', $data);
             return $this->db->insert_id();
-            
+
         } finally {
             if (isSet($this->db))
                 $this->db->close();
         }
     }
-    
+
     public function listarTodos() {
         try {
 
@@ -54,30 +54,30 @@ class AtendimentoDal extends CI_Model {
             {
                 $atendimento = new Atendimento();
                 $atendimento->setId($row->atd_id);
-                
+
                 $cliente = new Cliente();
                 $cliente->setId($row->cli_id);
                 $atendimento->setCliente($cliente->jsonSerialize());
-                
+
                 $profissional = new Profissional();
                 $profissional->setId($row->pro_id);
                 $atendimento->setProfissional($profissional->jsonSerialize());
-                
+
                 $servico = new Servico();
                 $servico->setId($row->ser_id);
                 $atendimento->setServico($servico->jsonSerialize());
-                
+
                 $atendimento->setDataAgendado($row->atd_data_agendado);
                 $atendimento->setDataRealizado($row->atd_data_realizado);
                 $atendimento->setDataCadastro($row->atd_data_cadastro);
                 $atendimento->setEndereco($row->atd_endereco);
                 $atendimento->setBairro($row->atd_bairro);
                 $atendimento->setCep($row->atd_cep);
-                
+
                 $municipio = new Municipio();
                 $municipio->setId($row->mun_id);
                 $atendimento->setMunicipio($municipio->jsonSerialize());
-                
+
                 $atendimento->setStatus($row->atd_status);
                 $atendimento->setPreco($row->atd_preco);
                 $atendimento->setDesconto($row->atd_desconto);
@@ -91,7 +91,7 @@ class AtendimentoDal extends CI_Model {
 
                 $list[] = $atendimento->jsonSerialize();
             }
-            
+
             return $list;
 
         } finally {
@@ -99,50 +99,50 @@ class AtendimentoDal extends CI_Model {
                 $this->db->close();
         }
     }
-    
+
     public function listarNaoConcluidos($cliente_json) {
         try {
-            
+
             $cliente = Cliente::fromJson($cliente_json);
-            
+
             // Conectando ao banco de dados
             $this->load->database();
-            
+
             $this->db->select('*');
             $this->db->where('cli_id', $cliente->getId());
             $this->db->where('atd_status', 'A');
             //die(print_r($this->db->get_compiled_select('tb_atendimento')));
             $query = $this->db->get('tb_atendimento');
-            
+
             $list = null;
             foreach ($query->result() as $row)
             {
                 $atendimento = new Atendimento();
                 $atendimento->setId($row->atd_id);
-                
+
                 $cliente = new Cliente();
                 $cliente->setId($row->cli_id);
                 $atendimento->setCliente($cliente->jsonSerialize());
-                
+
                 $profissional = new Profissional();
                 $profissional->setId($row->pro_id);
                 $atendimento->setProfissional($profissional->jsonSerialize());
-                
+
                 $servico = new Servico();
                 $servico->setId($row->ser_id);
                 $atendimento->setServico($servico->jsonSerialize());
-                
+
                 $atendimento->setDataAgendado($row->atd_data_agendado);
                 $atendimento->setDataRealizado($row->atd_data_realizado);
                 $atendimento->setDataCadastro($row->atd_data_cadastro);
                 $atendimento->setEndereco($row->atd_endereco);
                 $atendimento->setBairro($row->atd_bairro);
                 $atendimento->setCep($row->atd_cep);
-                
+
                 $municipio = new Municipio();
                 $municipio->setId($row->mun_id);
                 $atendimento->setMunicipio($municipio->jsonSerialize());
-                
+
                 $atendimento->setStatus($row->atd_status);
                 $atendimento->setPreco($row->atd_preco);
                 $atendimento->setDesconto($row->atd_desconto);
@@ -156,7 +156,7 @@ class AtendimentoDal extends CI_Model {
 
                 $list[] = $atendimento->jsonSerialize();
             }
-            
+
             return $list;
 
         } finally {
@@ -164,50 +164,50 @@ class AtendimentoDal extends CI_Model {
                 $this->db->close();
         }
     }
-    
+
     public function listarConcluidos($cliente_json) {
         try {
-            
+
             $cliente = Cliente::fromJson($cliente_json);
-            
+
             // Conectando ao banco de dados
             $this->load->database();
-            
+
             $this->db->select('*');
             $this->db->where('cli_id', $cliente->getId());
             $this->db->where('atd_status', 'R');
             //die(print_r($this->db->get_compiled_select('tb_atendimento')));
             $query = $this->db->get('tb_atendimento');
-            
+
             $list = null;
             foreach ($query->result() as $row)
             {
                 $atendimento = new Atendimento();
                 $atendimento->setId($row->atd_id);
-                
+
                 $cliente = new Cliente();
                 $cliente->setId($row->cli_id);
                 $atendimento->setCliente($cliente->jsonSerialize());
-                
+
                 $profissional = new Profissional();
                 $profissional->setId($row->pro_id);
                 $atendimento->setProfissional($profissional->jsonSerialize());
-                
+
                 $servico = new Servico();
                 $servico->setId($row->ser_id);
                 $atendimento->setServico($servico->jsonSerialize());
-                
+
                 $atendimento->setDataAgendado($row->atd_data_agendado);
                 $atendimento->setDataRealizado($row->atd_data_realizado);
                 $atendimento->setDataCadastro($row->atd_data_cadastro);
                 $atendimento->setEndereco($row->atd_endereco);
                 $atendimento->setBairro($row->atd_bairro);
                 $atendimento->setCep($row->atd_cep);
-                
+
                 $municipio = new Municipio();
                 $municipio->setId($row->mun_id);
                 $atendimento->setMunicipio($municipio->jsonSerialize());
-                
+
                 $atendimento->setStatus($row->atd_status);
                 $atendimento->setPreco($row->atd_preco);
                 $atendimento->setDesconto($row->atd_desconto);
@@ -221,8 +221,26 @@ class AtendimentoDal extends CI_Model {
 
                 $list[] = $atendimento->jsonSerialize();
             }
-            
+
             return $list;
+
+        } finally {
+            if (isSet($this->db))
+                $this->db->close();
+        }
+    }
+
+    public function cancelar($atendimento_json) {
+        try {
+
+            $atendimento = Atendimento::fromJson($atendimento_json);
+            // Conectando ao banco de dados
+            $this->load->database();
+
+            $this->db->where('atd_id', $atendimento->getId());
+            $this->db->set('atd_status', 'C');
+
+            return $this->db->update('tb_atendimento');
 
         } finally {
             if (isSet($this->db))
